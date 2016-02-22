9410be62f8f64e94b5eeb4794331482530ff465f
linhtdao <daol@oregonstate.edu>
2016-02-21 22:59:29 -0800

    Added files via upload

---
+++ submit.js
@@ -0,0 +1,60 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+app.get('/',function(req,res){
+  var qParams = [];
+  for (var p in req.query){
+    qParams.push({'name':p,'value':req.query[p]})
+  }
+  var context = {};
+  context.dataList = qParams;
+  context.objectName = 'GET';
+  res.render('getPost.handlebars', context);
+});
+
+app.post('/', function(req,res){
+  var qParams = [];
+  for (var p in req.query) {
+  	qParams.push({'name':p, 'value':req.query[p]})
+  }
+
+  var bParams = [];
+  for (var p in req.body){
+    bParams.push({'bodyName':p,'bodyValue':req.body[p]})
+  }
+  //console.log(bParams);
+  //console.log(req.body);
+
+  var context = {};
+  context.dataList = qParams;
+  context.bDataList = bParams;
+  context.objectName = 'POST';
+  
+  res.render('getPost.handlebars', context);
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
